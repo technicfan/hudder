4829fd6502717c74e8c7d93fa43b0fb97615c139
NGSpace <71827944+NGSpace@users.noreply.github.com>
2026-03-24 21:46:41 +0200

    Reenable modmenu

--- src/main/java/dev/ngspace/hudder/config/HudderModMenuImpl.java
+++ src/main/java/dev/ngspace/hudder/config/HudderModMenuImpl.java
@@ -1,7 +1,12 @@
 package dev.ngspace.hudder.config;
 
-public class HudderModMenuImpl {//implements ModMenuApi {
-//	@Override public ConfigScreenFactory<Screen> getModConfigScreenFactory() {
-//		return HudderNGSMCConfigMenu::createMenu;
-//    }
+import com.terraformersmc.modmenu.api.ConfigScreenFactory;
+import com.terraformersmc.modmenu.api.ModMenuApi;
+
+import net.minecraft.client.gui.screens.Screen;
+
+public class HudderModMenuImpl implements ModMenuApi {
+	@Override public ConfigScreenFactory<Screen> getModConfigScreenFactory() {
+		return HudderNGSMCConfigMenu::createMenu;
+    }
 }
